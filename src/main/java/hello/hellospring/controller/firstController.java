de663d1fc334477e2e3e2dc9f56e04fa6f2c156f
장수현 <76679995+suhyeon10@users.noreply.github.com>
2022-05-18 22:21:53 +0900

    스프링 의존관계

---
+++ src/main/java/hello/hellospring/controller/firstController.java
@@ -0,0 +1,53 @@
+package hello.hellospring.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class firstController {
+    @GetMapping("Hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    //데이터를 그대로 내려줌
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    //static쓰면 클래스 안에서 쓸수잇음
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
